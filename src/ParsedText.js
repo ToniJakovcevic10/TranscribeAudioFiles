3a76ea3fb1bae37ed6f6b0c64aa2ff1e6b684baa
Toni Jakovcevic <toni.jakovcevic10@gmail.com>
2023-05-22 10:35:16 +0200

    test3

--- src/ParsedText.js
+++ src/ParsedText.js
@@ -12,7 +12,6 @@ function ParsedText() {
 			<p className='parsedTextParagraph'>here will parsed text be...</p>
 			<p className='parsedTextParagraph'>here will parsed text be...</p>
 			<p className='parsedTextParagraph'>here will parsed text be...</p>
-			<p className='parsedTextParagraph'>here will parsed text be...</p>
 		</div>
 	);
 }
